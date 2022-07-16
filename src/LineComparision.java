9a7e826be41d8b5303ac34d6468b95e10acf8381
Shubham <shubhamaryan805@gmail.com>
2022-07-16 14:08:10 +0530

    Added : Line comparision Problem solving Based on End Point

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -3,48 +3,44 @@ package com.blz.javaproject;
 import java.util.Scanner;
 
 public class LineComparision {
+    public static double lineValues() {
+        Scanner sc = new Scanner(System.in);
+        double x1,x2,y1,y2;
+
+        System.out.println("Enter Values For First Point");
+        x1 = sc.nextDouble();
+        y1 = sc.nextDouble();
+
+        System.out.println("Enter Values For Second Point");
+        x2 = sc.nextDouble();
+        y2 = sc.nextDouble();
+
+        double lineLength = (double) Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return lineLength;
+    }
+
+    public static void checkEquality(double lineOneLength , double lineTwoLength ) {
+        if(lineOneLength == lineTwoLength ) {
+            System.out.println("Lines are Equal");
+        }
+        else if(lineOneLength > lineTwoLength) {
+            System.out.println("line1 is greater than line2");
+        }
+        else {
+            System.out.println("Line1 is less than line2");
+        }
+    }
+
     public static void main(String[] args) {
-        //Scanner which is added to take values from the user
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-        Scanner Scan = new Scanner(System.in);
-        //Input values from the user
-        System.out.println("Enter the Values of the line X1 and Y1");
-        System.out.println("Enter the value of X1 :  ");
-        int X1 = Scan.nextInt();
-        System.out.println("Enter the value of Y1 :  ");
-        int Y1 = Scan.nextInt();
-        System.out.println("Enter the Values of the line X2 and Y2");
-        System.out.println("Enter the value of X2 :  ");
-        int X2 = Scan.nextInt();
-        System.out.println("Enter the value of Y2 :  ");
-        int Y2 = Scan.nextInt();
-        System.out.println("Enter the Values for the 2nd Line");
-        // Line Two Initiated values from User
-        System.out.println("Enter the Values of the line Y3 and X3");
-        System.out.println("Enter the value of X3 :  ");
-        int X3 = Scan.nextInt();
-        System.out.println("Enter the value of Y3 :  ");
-        int X4 = Scan.nextInt();
-        System.out.println("Enter the Values of the line Y4 and X4");
-        System.out.println("Enter the value of X4 :  ");
-        int Y3 = Scan.nextInt();
-        System.out.println("Enter the value of Y4 :  ");
-        int Y4 = Scan.nextInt();
+        System.out.println("Welcome to Line Comparison Computation ");
 
-        double length = Math.sqrt((X2 - X1) * (Y2 - Y1) + (Y2 - Y1) * (Y2 - Y1));
-        System.out.println("The length of the line is  : " +length);
+        double lineOneLength = lineValues();
+        System.out.println("Length Of Line1 is: " + lineOneLength);
 
-        double length2 = Math.sqrt((X4 - X3) * (Y4 - Y3) + (Y4 - Y3) * (Y4 - Y3));
-        System.out.println("The length of the line is  : " +length2);
+        double lineTwoLength = lineValues();
+        System.out.println("Length Of Line2 is: " + lineTwoLength);
 
-        if(length == length2)
-        {
-            System.out.println("The Lines Segments are Equal");
-        }
-        else{
-            System.out.println("The Lines Segments are not Equal");
-        }
-
+        checkEquality(lineOneLength , lineTwoLength);
     }
 
 }
